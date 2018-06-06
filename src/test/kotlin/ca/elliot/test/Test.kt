8cc3c29871e565e8f2debb70a1456bf9bcdf78cd
tapoo tapoo <laurendeau.elliot@gmail.com>
2018-06-06 15:33:30 -0400

    rebuild to fix the unit test

---
+++ src/test/kotlin/ca/elliot/test/Test.kt
@@ -0,0 +1,10 @@
+package ca.elliot.test
+
+import org.junit.Test
+
+
+class Test {
+    fun Test() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
