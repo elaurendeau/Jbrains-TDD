dd6db48fabc3577fe31b4125477b622d0125d194
tapoo tapoo <laurendeau.elliot@gmail.com>
2018-06-06 13:25:34 -0400

    gitingore for intellij, kotlin and gradle

---
+++ src/test/kotlin/ca/elaurendeau/jbrainstdd/setup/test/HookupTest.kt
@@ -0,0 +1,10 @@
+package ca.elaurendeau.jbrainstdd.setup.test
+
+class HookupTest {
+    fun isThisThingOn() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        mutableListOf(1,2).isEmpty()
+
+
+    }
+}
