3a8c6e225f80d0e031da137d68d0d1874f90fea5
tapoo tapoo <laurendeau.elliot@gmail.com>
2018-06-07 14:42:13 -0400

    adding equal test for different denominator

--- fractions/src/test/kotlin/ca/elliot/fractions/EqualTest.kt
+++ fractions/src/test/kotlin/ca/elliot/fractions/EqualTest.kt
@@ -9,9 +9,15 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EqualTest {
 
+    //    5/3 == 5/4
+    @Test
+    internal fun `invalid equal fractions different denominator`() {
+        assertThat(FractionDto(numerator = 5, denominator = 3).same(FractionDto(numerator = 5, denominator = 4))).isFalse()
+    }
+
     //    5/3 == 7/3
     @Test
-    internal fun `invalid equal fractions`() {
+    internal fun `invalid equal fractions different numerator`() {
         assertThat(FractionDto(numerator = 5, denominator = 3).same(FractionDto(numerator = 7, denominator = 3))).isFalse()
     }
     //    5/3 == 5/3
