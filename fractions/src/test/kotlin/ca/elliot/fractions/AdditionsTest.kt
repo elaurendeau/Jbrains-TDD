2b87e3b35590626859d4747c6c4ff461c17ff554
tapoo tapoo <laurendeau.elliot@gmail.com>
2018-06-07 14:17:04 -0400

    equal method implemented

--- fractions/src/test/kotlin/ca/elliot/fractions/AdditionsTest.kt
+++ fractions/src/test/kotlin/ca/elliot/fractions/AdditionsTest.kt
@@ -3,6 +3,7 @@ package ca.elliot.fractions
 import ca.elliot.fractions.constants.FractionConstants
 import ca.elliot.fractions.dto.FractionDto
 import ca.elliot.fractions.extensions.add
+import ca.elliot.fractions.extensions.same
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -13,46 +14,46 @@ class AdditionsTest {
     //    4/5 + 7/3 = 47/15
     @Test
     internal fun `valid addition of two positive numbers with different denominators`() {
-        val resultingFraction = FractionDto(numerator = 4, denominator = 5).add(FractionDto(numerator = 7, denominator = 3))
+        val expectedFraction = FractionDto(numerator = 47, denominator = 15)
+        val actualFraction = FractionDto(numerator = 4, denominator = 5).add(FractionDto(numerator = 7, denominator = 3))
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(47)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(15)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //   -2/4 + 1/3 = -2/12 = -1/6
     @Test
     internal fun `valid addition of one negative number and a positive number`() {
-        val resultingFraction = FractionDto(numerator = -2, denominator = 4).add(FractionDto(numerator = 1, denominator = 3))
+        val expectedFraction = FractionDto(numerator = -1, denominator = 6)
+        val actualFraction = FractionDto(numerator = -2, denominator = 4).add(FractionDto(numerator = 1, denominator = 3))
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(-2)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(12)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    0/0 + 0/0 = 0/0
     @Test
     internal fun `valid addition of two zero numbers`() {
-        val resultingFraction = FractionDto(numerator = 0, denominator = 0).add(FractionDto(numerator = 0, denominator = 0))
+        val expectedFraction = FractionDto(numerator = 0, denominator = 0)
+        val actualFraction = FractionDto(numerator = 0, denominator = 0).add(FractionDto(numerator = 0, denominator = 0))
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(0)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(0)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    1/2 + 0/0 = 1/2
     @Test
     internal fun `valid addition of a zero and a left positive number`() {
-        val resultingFraction = FractionDto(numerator = 0, denominator = 0).add(FractionDto(numerator = 1, denominator = 2))
+        val expectedFraction = FractionDto(numerator = 1, denominator = 2)
+        val actualFraction = FractionDto(numerator = 0, denominator = 0).add(FractionDto(numerator = 1, denominator = 2))
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(1)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(2)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    0/0 + 1/2 = 1/2
     @Test
     internal fun `valid addition of a zero and a right positive number`() {
-        val resultingFraction = FractionDto(numerator = 1, denominator = 2).add(FractionDto(numerator = 0, denominator = 0))
+        val expectedFraction = FractionDto(numerator = 1, denominator = 2)
+        val actualFraction = FractionDto(numerator = 1, denominator = 2).add(FractionDto(numerator = 0, denominator = 0))
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(1)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(2)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    1/0 + 1/2 = illegal argument
--- fractions/src/test/kotlin/ca/elliot/fractions/ReductionTest.kt
+++ fractions/src/test/kotlin/ca/elliot/fractions/ReductionTest.kt
@@ -1,7 +1,9 @@
 package ca.elliot.fractions
 
 import ca.elliot.fractions.dto.FractionDto
+import ca.elliot.fractions.extensions.add
 import ca.elliot.fractions.extensions.reduce
+import ca.elliot.fractions.extensions.same
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -12,53 +14,53 @@ class ReductionTest {
     //    4/3 = 4/3
     @Test
     internal fun `valid reduction that can't be reduced`() {
-        val resultingFraction = FractionDto(numerator = 4, denominator = 3).reduce()
+        val expectedFraction = FractionDto(numerator = 4, denominator = 3)
+        val actualFraction = FractionDto(numerator = 4, denominator = 3)
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(4)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(3)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    2/12 = 1/6
     @Test
     internal fun `valid reduction with smaller numerator`() {
-        val resultingFraction = FractionDto(numerator = 2, denominator = 12).reduce()
+        val expectedFraction = FractionDto(numerator = 1, denominator = 6)
+        val actualFraction = FractionDto(numerator = 2, denominator = 12)
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(1)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(6)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    15/3 = 5/1
     @Test
     internal fun `valid reduction with bigger numerator`() {
-        val resultingFraction = FractionDto(numerator = 15, denominator = 3).reduce()
+        val expectedFraction = FractionDto(numerator = 5, denominator = 1)
+        val actualFraction = FractionDto(numerator = 15, denominator = 3)
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(5)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(1)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    -12/3 = -4/1
     @Test
     internal fun `valid reduction with a negative numerator`() {
-        val resultingFraction = FractionDto(numerator = -12, denominator = 3).reduce()
+        val expectedFraction = FractionDto(numerator = -4, denominator = 1)
+        val actualFraction = FractionDto(numerator = -12, denominator = 3)
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(-4)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(1)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
     //    0/0 = 0/0
     @Test
     internal fun `valid reduction of zero`() {
-        val resultingFraction = FractionDto(numerator = 0, denominator = 0).reduce()
+        val expectedFraction = FractionDto(numerator = 0, denominator = 0)
+        val actualFraction = FractionDto(numerator = 0, denominator = 0)
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(0)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(0)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 
     //    0/1 = 0/0
     @Test
     internal fun `valid reduction of zero numerator and a non zero denominator`() {
-        val resultingFraction = FractionDto(numerator = 0, denominator = 1).reduce()
+        val expectedFraction = FractionDto(numerator = 0, denominator = 0)
+        val actualFraction = FractionDto(numerator = 0, denominator = 1)
 
-        Assertions.assertThat(resultingFraction.numerator).isEqualTo(0)
-        Assertions.assertThat(resultingFraction.denominator).isEqualTo(0)
+        Assertions.assertThat(actualFraction.same(expectedFraction)).isTrue()
     }
 }
